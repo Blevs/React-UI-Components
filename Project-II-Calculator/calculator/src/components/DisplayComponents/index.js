e533ca472bf83b209574931c4e504fb1de79cdb7
Henry Blevins <heblevi@gmail.com>
2019-03-19 13:48:25 -0400

    Fix display overflow
    Note that this is harcoded to the calculator max width

--- Project-II-Calculator/calculator/src/components/DisplayComponents/index.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Display.css';
 
 const Display = ({total}) => {
-    return <div className="display">{total}</div>;
+    const length = total.toString().length,
+          value = (length > 11
+                   ? (total >= 99999999999 ? total.toExponential(5) : total.toPrecision(10))
+                   : total);
+    return <div className="display">{value}</div>;
 };
 
 export default Display;
